79c9fdb45c32f4e3df461edf00ba11deaee556fb
Dhanush S <dhanushs3366@gmail.com>
2024-04-14 12:48:10 +0530

    views/consts->consts/

---
+++ consts/PageClasses.go
@@ -0,0 +1,10 @@
+package consts
+
+const ChildrenBody="justify-start w-min-1/2 w-1/2 bg-gray-200  py-2"
+const BlueButton="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+
+const FormLabel="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+const FormInput="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+const Form="w-3/4 mt-1 mx-auto space-y-4"
+
+const FontText="text-gray-900 dark:text-white"
